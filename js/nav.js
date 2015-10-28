3aa1fdc8cfa1ed8010e5415067936ab464de8eb6
Riccardo Zaffalon <riccardo.zaffalon@gmail.com>
2015-10-28 16:29:36 +0100

    Version 1.0
    First commit

---
+++ js/nav.js
@@ -0,0 +1,19 @@
+$("nav ul li a").on("click", function( e ) {
+    
+    e.preventDefault();
+
+    $('.full-nav').removeClass('is-open');
+
+    $('#toggle-menu input').attr('checked', false);
+
+    $("body, html").animate({ 
+        scrollTop:$($(this).attr('href')).offset().top 
+    }, 1000);
+    
+});
+
+$('#toggle-menu input').on("click", function(e){
+
+	$('.full-nav').toggleClass('is-open');
+
+});
